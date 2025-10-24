b74320763d7fa37b7d83e9fe95d665fbfa8c736f
LexterDev <107295770+LexterDev@users.noreply.github.com>
2025-10-23 21:39:05 -0600

    Remove Auth0 credentials from environment file
    Clear Auth0 domain and client ID for environment.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080/api',
     auth0: {
-        auth0Domain: 'alexgalicia.us.auth0.com',
-        auth0ClientId: '386kEeAp1zbaYEwBDIvObVI9wyxITND2',
+        auth0Domain: '',
+        auth0ClientId: '',
     }
 }
